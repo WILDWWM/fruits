44420d39482fc6dd5b5316d111180fbf5a6cbd1b
WILDWWM <wpetit@cosformation.fr>
2022-10-25 12:12:13 +0200

    Modif avec dyn

--- mainbis.js
+++ mainbis.js
@@ -1,11 +1,22 @@
-const fruits = [{ name: "Orange", image: "./images/orange.jpg" }, { name: "Kiwi", image: "./images/kiwi.jpg" }, { name: "Banane", image: "./images/banane.jpg" }, { name: "Cerise", image: "./images/cerise.jpg" }];
-
+const fruits = [{ name: "Orange", image: "./images/orange.jpg" }, { name: "Kiwi", image: "./images/kiwi.jpg" }, { name: "Banane", image: "./images/banane.jpg" }, { name: "Peche", image: "./images/peche.jpg" }, { name: "Cerise", image: "./images/cerise.jpg" }];
 const photo = document.getElementById("photoBlock");
 
-fruits.forEach(fruit => {  
-    const monFruit = document.getElementById(fruit.name);    
+for (let i = 0; i < fruits.length; i++) {
+    let maDiv = document.getElementById("container_fruit");
+    let laDivFruit = document.createElement("div");
+    laDivFruit.classList.add("rectangle");
+    maDiv.appendChild(laDivFruit);
+    let monTitre = document.createElement("h1");
+    monTitre.setAttribute("id", fruits[i].name)
+    monTitre.textContent = fruits[i].name;
+    laDivFruit.appendChild(monTitre);
+}
+
+fruits.forEach(fruit => {
+
+    const monFruit = document.getElementById(fruit.name);
     monFruit.addEventListener("click", event => {
         photo.style.backgroundImage = `url(${fruit.image})`
-    });        
-});        
+    });
+});
 
