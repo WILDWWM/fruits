b7c52aeca755b39b978a4d694f322ca4a5730f50
WILDWWM <wpetit@cosformation.fr>
2022-10-25 16:36:14 +0200

    Modif_004

--- mainbis.js
+++ mainbis.js
@@ -1,22 +1,18 @@
 const fruits = [{ name: "Orange", image: "./images/orange.jpg" }, { name: "Kiwi", image: "./images/kiwi.jpg" }, { name: "Banane", image: "./images/banane.jpg" }, { name: "Peche", image: "./images/peche.jpg" }, { name: "Cerise", image: "./images/cerise.jpg" }];
 const photo = document.getElementById("photoBlock");
 
-for (let i = 0; i < fruits.length; i++) {
+fruits.forEach(fruit => { 
     let maDiv = document.getElementById("container_fruit");
     let laDivFruit = document.createElement("div");
     laDivFruit.classList.add("rectangle");
     maDiv.appendChild(laDivFruit);
     let monTitre = document.createElement("h1");
-    monTitre.setAttribute("id", fruits[i].name)
-    monTitre.textContent = fruits[i].name;
+    laDivFruit.setAttribute("id", fruit.name)
+    monTitre.textContent = fruit.name;
     laDivFruit.appendChild(monTitre);
-}
-
-fruits.forEach(fruit => {
-
     const monFruit = document.getElementById(fruit.name);
     monFruit.addEventListener("click", event => {
         photo.style.backgroundImage = `url(${fruit.image})`
-    });
+        monFruit.style.backgroundColor = "#339933";                
+    });    
 });
-
