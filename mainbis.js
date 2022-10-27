865e89f46adaaf921142c7a37da440add525154d
WILDWWM <wpetit@cosformation.fr>
2022-10-27 09:08:12 +0200

    Envoi 2

--- mainbis.js
+++ mainbis.js
@@ -1,8 +1,8 @@
 const fruits = [{ name: "Orange", image: "./images/orange.jpg" }, { name: "Kiwi", image: "./images/kiwi.jpg" }, { name: "Banane", image: "./images/banane.jpg" }, { name: "Peche", image: "./images/peche.jpg" }, { name: "Cerise", image: "./images/cerise.jpg" }];
 const photo = document.getElementById("photoBlock");
+let maDiv = document.getElementById("container_fruit");
 
-fruits.forEach(fruit => { 
-    let maDiv = document.getElementById("container_fruit");
+fruits.forEach(fruit => {     
     let laDivFruit = document.createElement("div");
     laDivFruit.classList.add("rectangle");
     maDiv.appendChild(laDivFruit);
@@ -10,9 +10,14 @@ fruits.forEach(fruit => {
     laDivFruit.setAttribute("id", fruit.name)
     monTitre.textContent = fruit.name;
     laDivFruit.appendChild(monTitre);
-    const monFruit = document.getElementById(fruit.name);
+    const monFruit = document.getElementById(fruit.name);    
     monFruit.addEventListener("click", event => {
+        let boutons = document.querySelectorAll(".rectangle");
+        boutons.forEach(bouton => {
+            bouton.style.backgroundColor = "";
+        })
         photo.style.backgroundImage = `url(${fruit.image})`
-        monFruit.style.backgroundColor = "#339933";                
-    });    
+        monFruit.style.backgroundColor = "#339933"; 
+        
+    });       
 });
